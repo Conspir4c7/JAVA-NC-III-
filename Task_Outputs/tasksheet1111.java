8c98437a6c9c458efd567ab9995cef0835d77741
Lucky Lusterio <lusterio202.0493@gmail.com>
2025-08-31 00:52:35 +0000

    Initial commit

---
+++ Task_Outputs/tasksheet1111.java
@@ -0,0 +1,10 @@
+package Task_Outputs;
+/** 
+ * The tasksheet1111 implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
+ */
+ public class tasksheet1111 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
