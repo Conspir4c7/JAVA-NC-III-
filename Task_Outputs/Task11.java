2005dabc01d6d4cf81e504586bdefc125bfd28f5
Lucky Lusterio <lusterio202.0493@gmail.com>
2025-09-16 08:57:40 +0000

    Initial Commit

--- Task_Outputs/Task11.java
+++ Task_Outputs/Task11.java
@@ -1,34 +1,35 @@
 package Task_Outputs;
-public class Task11{
+
+public class Task11 {
     public static void main(String[] args) {
-    Book book1 = new Book("Java Programming","John Smith", 2021, 39.99);
-    Book book2 = new Book("Python Basics", "Jane Doe", 2020, 29.99);
-    Book book3 = new Book("C++ Essentials" , "Michael Johnson", 2019, 49.99);
+        Book book1 = new Book("Java Programming","John Smith", 2021, 39.99);
+        Book book2 = new Book("Python Basics", "Jane Doe", 2020, 29.99);
+        Book book3 = new Book("C++ Essentials" , "Michael Johnson", 2019, 49.99);
+
         System.out.println(book1);
-    }
-    
-class Book{
-String title;
-String author;
-int yearPublished;
-double price;
-
-
-
-    Book(String title, String author, int yearPublished, double price){
-    this.title = title;
-    this.author = author;
-    this.yearPublished = yearPublished;
-    this.price = price;
+        System.out.println(book2);
+        System.out.println(book3);
     }
 }
 
+class Book {
+    String title;
+    String author;
+    int yearPublished;
+    double price;
 
-
-
+    Book(String title, String author, int yearPublished, double price){
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
 
     
-
-
-
-
+    public String toString() {
+        return "Author: " + author + "\n"
+             + "Title: " + title + "\n"
+             + "Year Published: " + yearPublished + "\n"
+             + "Price: $" + price;
+    }
+}
