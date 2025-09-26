3232e37d6511cb858accc6d9e4f715f80fab2501
Lucky Lusterio <lusterio202.0493@gmail.com>
2025-09-26 15:12:00 +0000

    Initial Commit

--- Task_Outputs/Task13.java
+++ Task_Outputs/Task13.java
@@ -1,27 +1,3 @@
-/*
- * 1. CREATE A BASE CLASS CALLED VEHICLE WITH THE FOLLOWING
- * ATTRIBUTES
- * make (String): The make of the vehicles
- * model (String): The model of the vehicles
- * year (int): The manufacturing year of the vehicle
- * 2. CREATE A DERIVED CLASS CALLED CAR THAT EXTENDS THE VEHICLE
- * CLASS. THE CAR CLASS SHOULD HAVE AN ADDITIONAL ATTRIBUTE:
- * numberOfDoors(int): The number of doors in the car
- * 3. CREATE AN INSTANCE OF THE CAR CLASS AND SET VALUES FOR ITS ATTRIBUTES.
- * 4. IMPLEMENT A METHOD IN THE CAR CLASS displayDetails() THAT PRINTS
- * OUT THE DETAILS OF THE CAR, INCLUDING THE INHERITED ATTRIBUTES
- * FROM THE VEHICLE CLASS
- * 
- * SAMPLE OUTPUT
- * 
- * Car Details:
- * Make: Toyota
- * Model: Camry
- * Year: 2022
- * Number of Doors: 4
- * 
- */
-
 class Vehicle {
     String make;
     String model;
